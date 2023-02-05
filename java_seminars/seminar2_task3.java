45b260facb53972716550b59fcb80b52b01369c1
Olesya Solovyova <soe611@yandex.ru>
2023-02-05 21:09:58 +0300

    added task3 sem2 - edits: added second option

--- java_seminars/seminar2_task3.java
+++ java_seminars/seminar2_task3.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class seminar2_task3 {
     public static void main(String[] args) {
         /*
-         * Напишите метод, который принимает на вход строку (String) и определяет,
-         * является ли строка палиндромом (возвращает boolean)
+         * Напишите метод, который принимает на вход строку (String)
+         * и определяет, является ли строка палиндромом (возвращает boolean)
          */
         Scanner iScanner = new Scanner(System.in);
         System.out.println("Type your word: ");
@@ -15,6 +15,10 @@ public class seminar2_task3 {
         // String line = "oasdsao";
         int n = line.length();
         boolean palin = false;
+        boolean palin2 = false;
+
+        // ------------Первый вариант решения----------------------------------
+
         for (int i = 0; i < n / 2; i++) {
             if (line.charAt(i) == line.charAt(n - 1 - i)) {
                 palin = true;
@@ -23,7 +27,18 @@ public class seminar2_task3 {
                 break;
             }
         }
-        if (palin) {
+
+        // ------------Второй вариант решения----------------------------------
+
+        String line2 = "";
+        for (int i = 0; i < n; i++) {
+            line2 += line.charAt(n - 1 - i);
+        }
+        if (line2.equalsIgnoreCase(line)) {
+            palin2 = true;
+        }
+
+        if (palin & palin2) {
             System.out.println("This is a palindrome");
         } else {
             System.out.println("This is NOT a palindrome");
